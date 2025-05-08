167b680757cc3db3f74c3b13f864f9ccfcddd2e2
GayathreeNetha <105914591+GayathreeNetha@users.noreply.github.com>
2025-05-08 23:24:31 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,22 @@
+class B
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name :Gayathree\nSurname : Dontha\nLocation : Hyderabad");
+		
+		
+	}
+
+}
+
+
+/*
+
+D:\HTML\Java Task>javac B.java
+
+D:\HTML\Java Task>java B
+Name :Gayathree
+Surname : Dontha
+Location : Hyderabad
+
+*/

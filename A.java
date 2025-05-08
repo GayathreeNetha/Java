04dcd2c8c10d88377fd61aabf4bccfd4db1ee847
GayathreeNetha <105914591+GayathreeNetha@users.noreply.github.com>
2025-05-08 23:22:13 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,24 @@
+class A
+{
+
+	public static void main(String[] args)
+	{
+		System.out.print("College Name : St.peter's Engineering College \nBranch : Electronics and Communication Enigneering\nLocation : Hyderabad ");
+		
+	}
+
+}
+
+
+/*
+
+D:\HTML\Java Task>javac A.java
+
+D:\HTML\Java Task>javac A.java
+
+D:\HTML\Java Task>java A
+College Name : St.peter's Engineering College
+Branch : Electronics and Communication Enigneering
+Location : Hyderabad
+
+*/

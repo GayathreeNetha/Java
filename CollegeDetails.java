168dc32b4e51a189ef58a461f7156c829bf76b60
GayathreeNetha <105914591+GayathreeNetha@users.noreply.github.com>
2025-05-08 22:18:47 +0530

    Add files via upload

---
+++ CollegeDetails.java
@@ -0,0 +1,13 @@
+class A
+{
+
+	
+	public static void main(String[] args)
+	{
+		System.out.println("CollegeNmae :St.peter's Engineering College");
+		System.out.println("Branch : Electronics and Communication Enigneering");
+		System.out.println("Location : Hyderabad");
+		
+	}
+
+}
---
+++ familydetails.java
@@ -0,0 +1,20 @@
+class B
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name :Gayathree");
+		System.out.println("Surname : Dontha");
+		System.out.println("Location : Hyderabad");
+		
+	}
+
+}
+
+/*
+ D:\HTML\Java Task>javac familydetails.java
+
+D:\HTML\Java Task>java B
+Name :Gayathree
+Surname : Dontha
+Location : Hyderabad
+*/

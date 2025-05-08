14e5e5881cb32dfb822f00aa0ac1234f97b6caa9
GayathreeNetha <105914591+GayathreeNetha@users.noreply.github.com>
2025-05-08 23:50:15 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,29 @@
+class Pattern
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"");
+		System.out.println("\"      *  *   \"");
+		System.out.println("\"        *    \"");
+		System.out.println("\"      * * *  \"");
+		System.out.println("\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"");
+		
+		
+	}
+
+}
+
+
+/*
+
+
+D:\HTML\Java Task>javac pattern.java
+
+D:\HTML\Java Task>java Pattern
+"""""""""""""""
+"      *  *   "
+"        *    "
+"      * * *  "
+"""""""""""""""
+
+*/
